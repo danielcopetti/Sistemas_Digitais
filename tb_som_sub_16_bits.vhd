2745282405921a548ca54ee495d71667bf461681
danielcopetti <37198301+danielcopetti@users.noreply.github.com>
2019-10-02 23:27:13 -0300

    Update tb_som_sub_16_bits.vhd

--- tb_som_sub_16_bits.vhd
+++ tb_som_sub_16_bits.vhd
@@ -5,11 +5,11 @@ entity TB_somador is
 end TB_somador;
 
 architecture teste of TB_somador is 
-	signal TB_A: std_logic_vector(15 downto 0);
-    signal TB_B: std_logic_vector(15 downto 0);
-    signal TB_control: std_logic;
-    signal TB_result: std_logic_vector(15 downto 0);
-    signal TB_cout: std_logic;
+	signal TB_A		: std_logic_vector(15 downto 0);
+    	signal TB_B		: std_logic_vector(15 downto 0);
+    	signal TB_control	: std_logic;
+    	signal TB_result	: std_logic_vector(15 downto 0);
+    	signal TB_cout		: std_logic;
 
 begin
 
@@ -20,19 +20,19 @@ begin
 --SOMA--
     	TB_control <= '0';
 
-		TB_A <= "0000000000001011";
+	TB_A <= "0000000000001011";
         TB_B <= "0000000000000101";
         wait for 10 ns;
 
-		TB_A <= "0000000000000101";
+	TB_A <= "0000000000000101";
         TB_B <= "0000000000001010";
         wait for 10 ns;
 
-		TB_A <= "0000000000000101";
+	TB_A <= "0000000000000101";
         TB_B <= "0000000000001011";
         wait for 10 ns;
 
-		TB_A <= "0000000000001111";
+	TB_A <= "0000000000001111";
         TB_B <= "0000000000000001";
         wait for 10 ns;
         
@@ -43,15 +43,15 @@ begin
 --SUBTRACAO--
         TB_control <= '1';
 
-		TB_A <= "0000000000000001";
+	TB_A <= "0000000000000001";
         TB_B <= "0000000000001111";      
         wait for 10 ns;
 
-		TB_A <= "0000000000001111";
+	TB_A <= "0000000000001111";
         TB_B <= "0000000000001111";      
         wait for 10 ns;
 
-		TB_A <= "0000000000000111";
+	TB_A <= "0000000000000111";
         TB_B <= "0000000000001111";      
         wait for 10 ns;
 
