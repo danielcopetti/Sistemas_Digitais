8c4999fdfc97a42ba023a0074644a1a8495ca200
danielcopetti <37198301+danielcopetti@users.noreply.github.com>
2019-10-02 23:24:38 -0300

    Update som_sub_16_bits.vhd

--- som_sub_16_bits.vhd
+++ som_sub_16_bits.vhd
@@ -3,19 +3,18 @@ use IEEE.std_logic_1164.all;
 
 entity sum_sub_16bits is
 	port(
-      ent_A, ent_B: in std_logic_vector(15 downto 0);
-        control   : in std_logic;
-      	result    : out std_logic_vector(15 downto 0);
-        cout      : out std_logic
-    );
+      		ent_A, ent_B	: in std_logic_vector(15 downto 0);
+        	control   	: in std_logic;
+      		result    	: out std_logic_vector(15 downto 0);
+        	cout      	: out std_logic
+    	);
 end entity;
 
 architecture struct of sum_sub_16bits is
 	signal sinalC: std_logic_vector(15 downto 0);
 	signal sinalB: std_logic_vector(15 downto 0);
-	begin
-    
-    sinalB(0)  <=  	ent_B(0) xor control;
+begin  
+    	sinalB(0)  <=  	ent_B(0) xor control;
 	sinalB(1)  <=  	ent_B(1) xor control;    
 	sinalB(2)  <=  	ent_B(2) xor control;
 	sinalB(3)  <=  	ent_B(3) xor control;
@@ -33,14 +32,12 @@ architecture struct of sum_sub_16bits is
 	sinalB(15) <=  ent_B(15) xor control;
     
     
-	SC0:  entity work.sum_sub_16bits port map(a => ent_A(0),  b => sinalB(0),  cin => control,   		s => result(0),  cout => sinalC(0));
-    SC1:  entity work.sum_sub_16bits port map(a => ent_A(1),  b => sinalB(1),  cin => sinalC(0),	s => result(1),  cout => sinalC(1));
+	SC0:  entity work.sum_sub_16bits port map(a => ent_A(0),  b => sinalB(0),  cin => control,   	s => result(0),  cout => sinalC(0));
+    	SC1:  entity work.sum_sub_16bits port map(a => ent_A(1),  b => sinalB(1),  cin => sinalC(0),	s => result(1),  cout => sinalC(1));
 	SC2:  entity work.sum_sub_16bits port map(a => ent_A(2),  b => sinalB(2),  cin => sinalC(1), 	s => result(2),  cout => sinalC(2));
 	SC3:  entity work.sum_sub_16bits port map(a => ent_A(3),  b => sinalB(3),  cin => sinalC(2), 	s => result(3),  cout => sinalC(3));
 	SC4:  entity work.sum_sub_16bits port map(a => ent_A(4),  b => sinalB(4),  cin => sinalC(3), 	s => result(4),  cout => sinalC(4));
-	
-    SC5:  entity work.sum_sub_16bits port map(a => ent_A(5),  b => sinalB(5),  cin => sinalC(4), 	s => result(5),  cout => sinalC(5));
-    
+	SC5:  entity work.sum_sub_16bits port map(a => ent_A(5),  b => sinalB(5),  cin => sinalC(4), 	s => result(5),  cout => sinalC(5));
 	SC6:  entity work.sum_sub_16bits port map(a => ent_A(6),  b => sinalB(6),  cin => sinalC(5), 	s => result(6),  cout => sinalC(6));
 	SC7:  entity work.sum_sub_16bits port map(a => ent_A(7),  b => sinalB(7),  cin => sinalC(6),	s => result(7),  cout => sinalC(7));
 	SC8:  entity work.sum_sub_16bits port map(a => ent_A(8),  b => sinalB(8),  cin => sinalC(7),	s => result(8),  cout => sinalC(8));
